04fff57d2c632653aaf40e1a447070c9f78f7281
Irfan Farras <irfanfarras10@gmail.com>
2022-05-15 22:36:33 +0700

    feat: add skeleton project

---
+++ src/test/java/id/myevent/MyeventApplicationTests.java
@@ -0,0 +1,13 @@
+package id.myevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
