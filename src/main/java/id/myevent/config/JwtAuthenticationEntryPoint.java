08b1c9e501cab581c949997fffead78ecee4e373
Irfan Farras <irfanfarras10@gmail.com>
2022-05-28 22:42:12 +0700

    test: test checkstyle

--- src/main/java/id/myevent/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/id/myevent/config/JwtAuthenticationEntryPoint.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+
 import org.springframework.stereotype.Component;
 
 /** Auth entry point for automatically throw unauthorized (401) HTTP error. */
