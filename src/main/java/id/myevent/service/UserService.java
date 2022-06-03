f0917793824f22a5697c802ab2f058594012c7ea
Irfan Farras <irfanfarras10@gmail.com>
2022-06-03 20:57:26 +0700

    fix: fix unused import

--- src/main/java/id/myevent/service/UserService.java
+++ src/main/java/id/myevent/service/UserService.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
