508ebbeff565e10ffa5c3d38101759047f897225
Irfan Farras <irfanfarras10@gmail.com>
2022-06-21 10:57:54 +0700

    feat: add ticket dao

---
+++ src/main/java/id/myevent/model/dao/TicketDao.java
@@ -0,0 +1,38 @@
+package id.myevent.model.dao;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/** Event DAO. */
+@Entity
+@Table(name = "ticket")
+@Data
+public class TicketDao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private long price;
+
+    @Column(name = "quota_per_day")
+    private long quotaPerDay;
+
+    @Column(name = "quota_total", nullable = false)
+    private long quotaTotal;
+
+    @ManyToOne()
+    @JoinColumn(name = "event_id", referencedColumnName = "id", nullable = false)
+    private EventDao event;
+}
