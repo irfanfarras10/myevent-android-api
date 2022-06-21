c7dbded8c6caa62b28fe27cc9bdf4e854d2c6400
Irfan Farras <irfanfarras10@gmail.com>
2022-06-21 12:16:44 +0700

    feat: add add ticket

---
+++ src/main/java/id/myevent/controller/TicketController.java
@@ -0,0 +1,31 @@
+package id.myevent.controller;
+
+import id.myevent.model.apiresponse.ApiResponse;
+import id.myevent.model.dto.TicketDto;
+import id.myevent.service.TicketService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Ticket REST Controller. */
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class TicketController {
+  @Autowired
+  TicketService ticketService;
+
+  @PostMapping("/events/{id}/ticket/create")
+  public ResponseEntity create(@PathVariable("id") Long id, @RequestBody TicketDto ticketDto) {
+    ticketService.create(id, ticketDto);
+    return new ResponseEntity(new ApiResponse("Tiket Tersimpan"), HttpStatus.CREATED);
+  }
+}
---
+++ src/main/java/id/myevent/model/dto/TicketDto.java
@@ -0,0 +1,14 @@
+package id.myevent.model.dto;
+
+import lombok.Data;
+
+/** Ticket DTO */
+@Data
+public class  TicketDto {
+  private String name;
+  private Long price;
+  private Integer dateTimeRegistrationStart;
+  private Integer dateTimeRegistrationEnd;
+  private Long quotaPerDay;
+  private Long quotaTotal;
+}
