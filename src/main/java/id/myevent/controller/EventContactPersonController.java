d916edd6b3d3375027d58c18a695b3d672b58eb5
alyannisa <alyannisa12@gmail.com>
2022-06-22 11:24:39 +0700

    feat: update contact person

--- src/main/java/id/myevent/controller/EventContactPersonController.java
+++ src/main/java/id/myevent/controller/EventContactPersonController.java
@@ -7,25 +7,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-/** Event Contact Person Controller. */
+/**
+ * Event Contact Person Controller.
+ */
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class EventContactPersonController {
-  @Autowired EventContactPersonService eventContactPersonService;
+    @Autowired
+    EventContactPersonService eventContactPersonService;
 
-  @PostMapping("/events/{id}/contact-person/create")
-  public ResponseEntity create(
-      @PathVariable Long id, @RequestBody EventContactPersonDto eventContactPersonDto) {
-    eventContactPersonService.create(id, eventContactPersonDto);
-    return new ResponseEntity(new ApiResponse("Contact Person Tersimpan"), HttpStatus.CREATED);
-  }
+    /** Create Contact Person*/
+    @PostMapping("/events/{id}/contact-person/create")
+    public ResponseEntity create(
+            @PathVariable Long id,
+            @RequestBody EventContactPersonDto eventContactPersonDto) {
+        eventContactPersonService.create(id, eventContactPersonDto);
+        return new ResponseEntity(new ApiResponse("Contact Person Tersimpan"), HttpStatus.CREATED);
+    }
+
+    /** Update Contact Person*/
+    @PutMapping("/events/{id}/contact-person/{id}")
+    public ResponseEntity<ApiResponse> updateCP(@PathVariable("id") Long eventId,
+                                                @PathVariable("id") Long cpId,
+                                                @RequestBody EventContactPersonDto eventContactPersonDto) {
+        eventContactPersonService.update(eventId, cpId, eventContactPersonDto);
+        return new ResponseEntity(new ApiResponse("Contact Person Berhasil di Update"), HttpStatus.CREATED);
+    }
 }
--- src/main/java/id/myevent/service/EventContactPersonService.java
+++ src/main/java/id/myevent/service/EventContactPersonService.java
@@ -9,7 +9,10 @@ import id.myevent.repository.EventContactPersonRepository;
 import id.myevent.repository.EventRepository;
 import id.myevent.repository.EventSocialMediaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /** Event Contact Person Service. */
 @Service
@@ -37,6 +40,33 @@ public class EventContactPersonService {
       eventContactPersonRepository.save(eventContactPerson);
     } catch (Exception e) {
       throw new ConflictException("Terjadi kesalahan pada saat menyimpan data");
+    }
+  }
+
+  /** Update Event Social Media. */
+  public void update(Long eventId, Long cpId, EventContactPersonDto cpData){
+    Optional<EventContactPersonDao> currentCP = eventContactPersonRepository.findById(cpId);
+    EventContactPersonDao newCP = currentCP.get();
+    final EventDao event = eventRepository.findById(eventId).get();
+
+    if(cpData.getName()!=null){
+      newCP.setName(cpData.getName());
+    }
+    if(cpData.getContact()!=null){
+      newCP.setContact(cpData.getContact());
+    }
+    if(cpData.getEventSocialMediaId()!=null){
+      final Optional<EventSocialMediaDao> eventSocialMedia =
+              eventSocialMediaRepository.findById(cpData.getEventSocialMediaId());
+      newCP.setEventSocialMedia(eventSocialMedia.get());
+    }
+    newCP.setEvent(event);
+
+    try {
+      eventContactPersonRepository.save(newCP);
+    } catch (DataIntegrityViolationException exception) {
+      String exceptionMessage = exception.getMostSpecificCause().getMessage();
+      throw new ConflictException(exceptionMessage);
     }
   }
 }
