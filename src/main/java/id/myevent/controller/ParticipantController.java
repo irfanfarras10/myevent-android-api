15046af8df35c8a0d3edbb7f65ea0f1b90972118
alyannisa <alyannisa12@gmail.com>
2022-07-07 15:21:36 +0700

    feat: view participants list and detail

--- src/main/java/id/myevent/controller/ParticipantController.java
+++ src/main/java/id/myevent/controller/ParticipantController.java
@@ -1,23 +1,33 @@
 package id.myevent.controller;
 
 import id.myevent.model.apiresponse.ApiResponse;
-import id.myevent.model.dto.EventGuestDto;
+import id.myevent.model.apiresponse.ViewEventApiResponse;
+import id.myevent.model.apiresponse.ViewEventListApiResponse;
+import id.myevent.model.apiresponse.ViewEventParticipantApiResponse;
+import id.myevent.model.apiresponse.ViewEventParticipantListApiResponse;
+import id.myevent.model.dao.EventDao;
+import id.myevent.model.dao.TicketParticipantDao;
 import id.myevent.model.dto.ParticipantDto;
 import id.myevent.service.ParticipantService;
+import id.myevent.util.ImageUtil;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * Participant Controller REST API.
+ */
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
@@ -27,6 +37,9 @@ public class ParticipantController {
   @Autowired
   ParticipantService participantService;
 
+  /**
+   * Participant Regist.
+   */
   @PostMapping("/events/{id}/participant/regist")
   public ResponseEntity create(
       @PathVariable("id") Long id,
@@ -43,15 +56,64 @@ public class ParticipantController {
     createParticipant.setEmail(email);
     createParticipant.setPhoneNumber(phoneNumber);
     createParticipant.setTicketId(ticketId);
-    if(paymentId != null){
+    if (paymentId != null) {
       createParticipant.setPaymentId(paymentId);
     }
-    if(paymentPhoto != null){
+    if (paymentPhoto != null) {
       createParticipant.setPaymentProofPhoto(paymentPhoto.getBytes());
       createParticipant.setPaymentPhotoType(paymentPhoto.getContentType());
     }
     createParticipant.setDateEvent(eventDate);
     participantService.create(id, createParticipant);
     return new ResponseEntity(new ApiResponse("Anda Berhasil Terdaftar"), HttpStatus.CREATED);
+  }
+
+  @GetMapping("/events/{eventId}/participants/confirmed")
+  public ViewEventParticipantListApiResponse getParticipantConfirmed(
+      @PathVariable("eventId") Long eventId) {
+    return participantService.getParticipantConfirmed(eventId);
+  }
+
+  @GetMapping("/events/{eventId}/participants/wait")
+  public ViewEventParticipantListApiResponse getParticipantWait(
+      @PathVariable("eventId") Long eventId) {
+    return participantService.getParticipantWait(eventId);
+  }
+
+  @GetMapping("/events/{eventId}/participants/attend")
+  public ViewEventParticipantListApiResponse getParticipantAttend(
+      @PathVariable("eventId") Long eventId) {
+    return participantService.getParticipantAttend(eventId);
+  }
+
+  /**
+   * get participant detail.
+   */
+  @GetMapping("events/{eventId}/participant/{participantId}")
+  public ViewEventParticipantApiResponse getDetailEvent(@PathVariable("eventId") Long eventId,
+                                                        @PathVariable("participantId")
+                                                        Long participantId) {
+    return participantService.getDetailParticipant(eventId, participantId);
+  }
+
+  /**
+   * get participant by name.
+   */
+  @GetMapping("events/{eventId}/participant/name")
+  public ViewEventParticipantListApiResponse getEventByName(@RequestParam("name") String name) {
+    return participantService.getParticipantByName(name);
+  }
+
+  /**
+   * get image participant.
+   */
+  @GetMapping(path = {"/events/participant/image/{name}"})
+  public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+
+    TicketParticipantDao image = participantService.getImage(name);
+
+    return ResponseEntity.ok()
+        .contentType(MediaType.valueOf(image.getPaymentPhotoType()))
+        .body(ImageUtil.decompressImage(image.getPaymentPhotoProof()));
   }
 }
--- src/main/java/id/myevent/service/ParticipantService.java
+++ src/main/java/id/myevent/service/ParticipantService.java
@@ -1,6 +1,11 @@
 package id.myevent.service;
 
 import id.myevent.exception.ConflictException;
+import id.myevent.model.apiresponse.EventData;
+import id.myevent.model.apiresponse.ViewEventApiResponse;
+import id.myevent.model.apiresponse.ViewEventListApiResponse;
+import id.myevent.model.apiresponse.ViewEventParticipantApiResponse;
+import id.myevent.model.apiresponse.ViewEventParticipantListApiResponse;
 import id.myevent.model.dao.EventDao;
 import id.myevent.model.dao.EventPaymentDao;
 import id.myevent.model.dao.ParticipantDao;
@@ -15,15 +20,21 @@ import id.myevent.repository.TicketRepository;
 import id.myevent.util.ImageUtil;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+/**
+ * Participant Service.
+ */
 @Service
 @Slf4j
 public class ParticipantService {
@@ -58,7 +69,11 @@ public class ParticipantService {
     participant.setName(participantData.getName());
     participant.setEmail(participantData.getEmail());
     participant.setPhoneNumber(participantData.getPhoneNumber());
-    participant.setStatus("Terdaftar");
+    if (eventData.getEventPaymentCategory().getId() == 1) {
+      participant.setStatus("Terkonfirmasi");
+    } else {
+      participant.setStatus("Menunggu Konfirmasi");
+    }
     participant.setEvent(eventData);
     try {
       participantRepository.save(participant);
@@ -83,14 +98,17 @@ public class ParticipantService {
     ticketParticipant.setEvent_date(participantData.getDateEvent());
     ticketParticipant.setPurchase_date(epochTime);
     ticketParticipant.setStatus("Terbayar");
-    ticketParticipant.setPaymentPhotoProof(ImageUtil.compressImage(participantData.getPaymentProofPhoto()));
-    ticketParticipant.setPaymentPhotoName(generateUniqueImageName(participantData.getPaymentPhotoName()));
+    ticketParticipant.setPaymentPhotoProof(
+        ImageUtil.compressImage(participantData.getPaymentProofPhoto()));
+    ticketParticipant.setPaymentPhotoName(
+        generateUniqueImageName(participantData.getPaymentPhotoName()));
     ticketParticipant.setPaymentPhotoType(participantData.getPaymentPhotoType());
     ticketParticipant.setTicket(ticketData);
-    if(participantData.getPaymentId() != null){
-      EventPaymentDao paymentData = eventPaymentRepository.findById(participantData.getPaymentId()).get();
+    if (participantData.getPaymentId() != null) {
+      EventPaymentDao paymentData =
+          eventPaymentRepository.findById(participantData.getPaymentId()).get();
       ticketParticipant.setEventPayment(paymentData);
-    }else{
+    } else {
       ticketParticipant.setEventPayment(null);
     }
     ticketParticipant.setParticipant(participant);
@@ -101,7 +119,7 @@ public class ParticipantService {
       throw new ConflictException(exceptionMessage);
     }
     //send email if event is free
-    if(eventData.getEventPaymentCategory().getId() == 1){
+    if (eventData.getEventPaymentCategory().getId() == 1) {
       emailService.sendMessage(eventId, participant.getId(), ticketParticipant.getId());
     }
   }
@@ -115,5 +133,97 @@ public class ParticipantService {
     String uuid = UUID.randomUUID().toString();
     filename = uuid + "_" + datetime + "_" + millis;
     return filename;
+  }
+
+  /**
+   * Get List of Participant Confirmed.
+   */
+  public ViewEventParticipantListApiResponse getParticipantConfirmed(Long eventId) {
+    List<ParticipantDao> participantEvent = participantRepository.findByStatusConfirmed(eventId);
+    ViewEventParticipantListApiResponse viewEventGuestListApiResponse =
+        new ViewEventParticipantListApiResponse();
+    viewEventGuestListApiResponse.setListParticipant(participantEvent);
+    return viewEventGuestListApiResponse;
+  }
+
+  /**
+   * Get List of Participant Wait.
+   */
+  public ViewEventParticipantListApiResponse getParticipantWait(Long eventId) {
+    List<ParticipantDao> participantEvent = participantRepository.findByStatusWait(eventId);
+    ViewEventParticipantListApiResponse viewEventGuestListApiResponse =
+        new ViewEventParticipantListApiResponse();
+    viewEventGuestListApiResponse.setListParticipant(participantEvent);
+    return viewEventGuestListApiResponse;
+  }
+
+
+  /**
+   * Get List of Participant Attend.
+   */
+  public ViewEventParticipantListApiResponse getParticipantAttend(Long eventId) {
+    List<ParticipantDao> participantEvent = participantRepository.findByStatusAttend(eventId);
+    ViewEventParticipantListApiResponse viewEventGuestListApiResponse =
+        new ViewEventParticipantListApiResponse();
+    viewEventGuestListApiResponse.setListParticipant(participantEvent);
+    return viewEventGuestListApiResponse;
+  }
+
+  /**
+   * Search Participant By Name.
+   */
+  public ViewEventParticipantListApiResponse getParticipantByName(String name) {
+    List<ParticipantDao> participant = participantRepository.findByName(name);
+    List<ParticipantDao> newParticipantData = new ArrayList<>();
+    ViewEventParticipantListApiResponse newParticipant = new ViewEventParticipantListApiResponse();
+
+    for (int i = 0; i < participant.size(); i++) {
+      ParticipantDao participants = new ParticipantDao();
+      participants.setId(participant.get(i).getId());
+      participants.setName(participant.get(i).getName());
+      participants.setEmail(participant.get(i).getEmail());
+      participants.setPhoneNumber(participant.get(i).getPhoneNumber());
+      participants.setStatus(participant.get(i).getStatus());
+      newParticipantData.add(participants);
+    }
+    newParticipant.setListParticipant(newParticipantData);
+    return newParticipant;
+  }
+
+  /**
+   * View Detail Participant.
+   */
+  public ViewEventParticipantApiResponse getDetailParticipant(Long eventId, Long participantId) {
+    ViewEventParticipantApiResponse newParticipant = new ViewEventParticipantApiResponse();
+    Optional<ParticipantDao> participantData = participantRepository.findById(participantId);
+    Optional<TicketParticipantDao> ticketData =
+        ticketParticipantRepository.findByParticipantId(participantId);
+
+    newParticipant.setId(participantData.get().getId());
+    newParticipant.setName(participantData.get().getName());
+    newParticipant.setEmail(participantData.get().getEmail());
+    newParticipant.setPhoneNumber(participantData.get().getPhoneNumber());
+    newParticipant.setTicket(ticketData.get().getTicket().getName());
+    newParticipant.setPaymentProofPhoto(
+        generateBannerPhotoUrl(ticketData.get().getPaymentPhotoName()));
+
+    return newParticipant;
+  }
+
+  private String generateBannerPhotoUrl(String filename) {
+    String url = "";
+    url = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+    url += "/api/events/participant/image/" + filename;
+    return url;
+  }
+
+  /**
+   * Get Participant Proof Image.
+   */
+  public TicketParticipantDao getImage(String imageName) {
+    final Optional<TicketParticipantDao> event =
+        ticketParticipantRepository.findByImageName(imageName);
+
+    return event.get();
   }
 }
